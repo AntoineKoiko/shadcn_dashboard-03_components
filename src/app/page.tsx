ab9693358b397820e2a6423569638bccca0607e3
AntoineKoiko <antoine.couacault@epitech.eu>
2024-06-21 16:26:00 +0200

    feat(app/page): make welcome page with link to access to dashboard and other link to shadcn block origin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          <h1>Welcome</h1>
-        </div>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col justify-center items-center p-8">
+            <h1 className="text-2xl absolute top-12 w-full text-center">
+                A <span className="font-bold">Shadcn/ui</span> based page
+            </h1>
+            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+                <div className="mx-auto grid columns-1 gap-8">
+                    <p className=" leading-loose sm:text-sm">
+                        A personal proposal for a breakdown into components of{" "}
+                        <Link
+                            href="https://ui.shadcn.com/blocks#dashboard-03"
+                            className="p-2 border-transparent rounded-md bg-slate-200 hover:bg-slate-300"
+                        >
+                            Dashboard-03
+                        </Link>{" "}
+                        block proprosed by Shadcn/ui{" "}
+                    </p>
+                    <Button className="max-w-2xl mx-auto">
+                        <a href="/dashboard">Go to Dashboard</a>
+                    </Button>
+                </div>
+            </div>
+        </main>
+    );
 }
